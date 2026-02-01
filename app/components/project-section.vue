245c09d3f5aaf40e0a7374b11d5856177a467e19
yash kashyap <yashkashyap1005@gmail.com>
2026-02-01 13:50:09 +0530

    fixed projects order

--- app/components/project-section.vue
+++ app/components/project-section.vue
@@ -8,13 +8,13 @@
       </h2>
 
       <div
-        v-for="project in projects"
+        v-for="project in projectsIndescendingOrder"
         :key="project.id"
         class="grid grid-cols-1 gap-5 py-14 md:grid-cols-2"
       >
         <div class="project-card aspect-auto -translate-x-20 opacity-0">
           <NuxtImg
-            :src="`./images/${project.id}.png`"
+            :src="`${project.imageUrl}`"
             class="rounded-xl"
             alt="nuxt-ai"
           />
@@ -74,14 +74,73 @@ interface projectTypes {
   tools: string[];
   appLink: string;
   githubLink: string;
+  imageUrl: string;
 }
 
 const projects = ref<projectTypes[]>([
   {
     id: 1,
+    title: "Responsive SCSS Hotel Landing Page",
+    description: "Stresses responsive design and the use of SCSS for styling.",
+    tools: ["Html", "SCSS"],
+    appLink: "https://yash-travel.netlify.app/",
+    githubLink: "#",
+    imageUrl: "./images/7.png",
+  },
+  {
+    id: 2,
+    title: "Modern Property Showcase Landing Page",
+    description: "Simple, professional title, emphasizing a showcase purpose.",
+    tools: ["Html", "SCSS", "javascript"],
+    appLink: "https://yash-property.netlify.app/",
+    githubLink: "#",
+    imageUrl: "./images/6.png",
+  },
+  {
+    id: 3,
+    title: "Animated SCSS/JS Tour Booking Page",
+    description:
+      "Highlights advanced, vanilla animation and the Tour Booking niche.",
+    tools: ["Html", "SCSS", "JavaScript"],
+    appLink: "https://yash-nature.netlify.app/",
+    githubLink: "#",
+    imageUrl: "./images/5.png",
+  },
+  {
+    id: 4,
+    title: "Real-Time Geo IP Tracker",
+    description:
+      "Focuses on real-time capability, location tracking, and map visualization.",
+    tools: ["Html", "Tailwind CSS", "JavaScript", "Vue.js"],
+    appLink: "https://location-ip-tracker.netlify.app/",
+    githubLink: "https://github.com/Enthusiastic-yash/Ip-Tracker",
+    imageUrl: "./images/4.png",
+  },
+  {
+    id: 5,
+    title: "Full-Stack Firebase Music Platform",
+    description:
+      "Highlights Full-Stack nature, user interaction (upload, listen, comment), and the key technologies (Vue.js, Tailwind, Firebase).",
+    tools: ["Html", "SCSS", "Firebase", "Vue.js"],
+    appLink: "https://music-example-ruddy.vercel.app/",
+    githubLink: "https://github.com/Enthusiastic-yash/Music-app",
+    imageUrl: "./images/3.png",
+  },
+  {
+    id: 6,
+    title: "Global Vue Weather Tracker",
+    description:
+      "Emphasizes real-time data, API integration, and the framework (Vue.js).",
+    tools: ["Html", "CSS", "JavaScript", "Vue.js"],
+    appLink: "https://global-weather-report.netlify.app/",
+    githubLink: "https://github.com/Enthusiastic-yash/weather-app",
+    imageUrl: "./images/2.png",
+  },
+  {
+    id: 7,
     title: "Full-Stack Nuxt AI Suite (SaaS)",
     description:
-      "A full-stack Software as a Service (SaaS) platform built with Nuxt 4 and Nuxt UI, offering a versatile collection of AI-powered tools. Key features include background removal, object removal from images using the Cloudinary API, a Resume Reviewer, Article Writer, and a Blog Title Generator. The application utilizes the Gemini/OpenAI API for core AI functionalities, manages user subscriptions via Polar, and ensures data persistence with the Turso database and Drizzle ORM for type-safe operations. Zod is used for robust runtime validation. and Better-Auth for secure authentication",
+      "A full-stack Software as a Service (SaaS) platform built with Nuxt 4 and Nuxt UI, offering a versatile collection of AI-powered tools. Key features include background removal, object removal from images using the Cloudinary API, a Resume Reviewer, Article Writer, and a Blog Title Generator. The application utilizes the Gemini/OpenAI API for core AI functionalities, manages user subscriptions via Polar, and ensures data persistence with the Turso database and Drizzle ORM for type-safe operations. Zod is used for robust runtime validation and Better-Auth for secure authentication.",
     tools: [
       "Nuxt",
       "NuxtUI",
@@ -96,52 +155,14 @@ const projects = ref<projectTypes[]>([
     ],
     appLink: "https://nuxt-ai-xi.vercel.app/",
     githubLink: "https://github.com/Enthusiastic-yash/Nuxt-ai-sass",
-  },
-  {
-    id: 2,
-    title: "Global Vue Weather Tracker",
-    description:
-      "Emphasizes real-time data, API integration, and the framework (Vue.js).",
-    tools: ["Html", "css", "javascript", "Vue.js"],
-    appLink: "https://global-weather-report.netlify.app/",
-    githubLink: "https://github.com/Enthusiastic-yash/weather-app",
-  },
-  {
-    id: 3,
-    title: "Full-Stack Firebase Music Platform",
-    description:
-      "Highlights Full-Stack nature, user interaction (upload, listen, comment), and the key technologies (Vue.js, Tailwind, Firebase).",
-    tools: ["Html", "SCSS", "Firebase", "Vue.js"],
-    appLink: "https://music-example-ruddy.vercel.app/",
-    githubLink: "https://github.com/Enthusiastic-yash/Music-app",
-  },
-  {
-    id: 4,
-    title: "Real-Time Geo IP Tracker",
-    description:
-      "Focuses on real-time capability, location tracking, and map visualization.",
-    tools: ["Html", "Tailwind CSS", "javascript", "Vue.js"],
-    appLink: "https://location-ip-tracker.netlify.app/",
-    githubLink: "https://github.com/Enthusiastic-yash/Ip-Tracker",
-  },
-  {
-    id: 5,
-    title: "Responsive SCSS Hotel Landing Page",
-    description: "Stresses responsive design and the use of SCSS for styling.",
-    tools: ["Html", "SCSS"],
-    appLink: "https://yash-nature.netlify.app/",
-    githubLink: "#",
-  },
-  {
-    id: 6,
-    title: "Animated SCSS/JS Tour Booking Page",
-    description:
-      "Highlights advanced, vanilla animation and the Tour Booking niche.",
-    tools: ["Html", "SCSS", "JavaScript"],
-    appLink: "https://yash-travel.netlify.app/",
-    githubLink: "#",
+    imageUrl: "./images/1.png",
   },
 ]);
+
+const projectsIndescendingOrder = computed(() => {
+  return projects.value.sort((a, b) => b.id - a.id);
+});
+
 const { $gsap } = useNuxtApp();
 
 let ctx: any;
